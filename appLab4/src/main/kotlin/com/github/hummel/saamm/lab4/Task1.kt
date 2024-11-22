bd9c3ae38edab476c7c11301a3048728548d3624
Hummel009 <hummel.turbamentum@gmail.com>
2024-11-22 13:00:50 +0300

    dover

--- appLab4/src/main/kotlin/com/github/hummel/saamm/lab4/Task1.kt
+++ appLab4/src/main/kotlin/com/github/hummel/saamm/lab4/Task1.kt
@@ -1,11 +1,15 @@
 package com.github.hummel.saamm.lab4
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction
+import org.apache.commons.math3.distribution.TDistribution
 import org.apache.commons.math3.fitting.PolynomialCurveFitter
 import org.apache.commons.math3.fitting.WeightedObservedPoints
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 import org.knowm.xchart.BitmapEncoder
 import org.knowm.xchart.BitmapEncoder.BitmapFormat
 import org.knowm.xchart.XYChart
+import org.knowm.xchart.XYSeries
+import java.awt.Color
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -21,11 +25,55 @@ fun researchCorrelation(statisticsArrayArray: Array<Array<Statistics>>) {
 
 	val xData = DoubleArray(yData.size) { i -> (i + 1) * 0.05 }
 
-	chart.addSeries("Оригинальные данные", xData, yData)
+	chart.addSeries("Оригинальные данные", xData, yData).apply {
+		xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+		markerColor = Color.GREEN
+		lineColor = Color.GREEN
+	}
 	val linearFit = linearApproximation(xData, yData)
-	chart.addSeries("Линейная аппроксимация", xData, linearFit)
+	chart.addSeries("Линейная аппроксимация", xData, linearFit).apply {
+		xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+		markerColor = Color.RED
+		lineColor = Color.RED
+	}
 	val polynomialFit = polynomialApproximation(xData, yData)
-	chart.addSeries("Полиномиальная аппроксимация", xData, polynomialFit)
+	chart.addSeries("Полиномиальная аппроксимация", xData, polynomialFit).apply {
+		xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+		markerColor = Color.RED
+		lineColor = Color.RED
+	}
+
+	val lowerBounds = mutableListOf<Double>()
+	val upperBounds = mutableListOf<Double>()
+
+	statisticsArrayArray.forEach { statsArray ->
+		val sampleStats = DescriptiveStatistics()
+		statsArray.forEach { stat -> sampleStats.addValue(stat.getProduceTime()) }
+
+		val mean = sampleStats.mean
+		val stdDev = sampleStats.standardDeviation
+		val n = sampleStats.n
+
+		val tDist = TDistribution(n - 1.0)
+		val alpha = 0.05
+		val tValue = tDist.inverseCumulativeProbability(1 - alpha / 2)
+
+		val marginOfError = tValue * (stdDev / sqrt(n.toDouble())) + 0.5
+
+		lowerBounds.add(mean - marginOfError)
+		upperBounds.add(mean + marginOfError)
+	}
+
+	chart.addSeries("Нижняя граница доверительного интервала", xData, lowerBounds.toDoubleArray()).apply {
+		xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+		markerColor = Color.BLUE
+		lineColor = Color.BLUE
+	}
+	chart.addSeries("Верхняя граница доверительного интервала", xData, upperBounds.toDoubleArray()).apply {
+		xySeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+		markerColor = Color.BLUE
+		lineColor = Color.BLUE
+	}
 
 	BitmapEncoder.saveBitmap(chart, "./$outputDir/task1", BitmapFormat.JPG)
 
